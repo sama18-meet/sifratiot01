038d3cc59c2066f77ec035514d15cb26a91a7d82
Sama Mazzawi <sama.mazz93@gmail.com>
2021-12-11 16:03:37 +0200

    Update mux2.sv

--- mux2.sv
+++ mux2.sv
@@ -6,10 +6,16 @@ module mux2 (
     output logic z           // Output
 );
 
-// Put your code here
-// ------------------
+logic g1, g2, g3, g4, g5, g6, g7, g8;
 
+or(z, g2, g3);
+not(g2, g4);
+or(g4, sel, g6);
+not(g6, d0);
+not(g3, g5);
+or(g5, g7, g8);
+not(g7, sel);
+not(g7, sel);
 
-// End of your code
 
 endmodule
