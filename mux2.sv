a0f0224d8675791c78e96146f0b938b510fe0da5
Sama Mazzawi <sama.mazz93@gmail.com>
2021-12-11 19:17:22 +0200

    Update mux2.sv

--- mux2.sv
+++ mux2.sv
@@ -6,19 +6,22 @@ module mux2 (
     output logic z           // Output
 );
 
+parameter TpdOr = 1;
+parameter TpdXor = 1;
+parameter TpdNot = 1;
 
 logic w2, w3, w4, w5, w6, w7, w8;
 
 
-OR2 #(.Tpdhl(10), .Tpdlh(1)) g1 (.A(w2), .B(w3), .Z(z));
-NOT #(.Tpdhl(1),  .Tpdlh(2)) g2 (.A(w4), .Z(w2));
-OR2 #(.Tpdhl(10), .Tpdlh(1)) g4 (.A(w6), .B(sel), .Z(w4));
-NOT #(.Tpdhl(1),  .Tpdlh(2)) g6 (.A(d0), .Z(w6));
+OR2 #(.Tpdhl(TpdOr), .Tpdlh(TpdOr)) g1 (.A(w2), .B(w3), .Z(z));
+NOT #(.Tpdhl(TpdNot),  .Tpdlh(TpdNot)) g2 (.A(w4), .Z(w2));
+OR2 #(.Tpdhl(TpdOr), .Tpdlh(TpdOr)) g4 (.A(w6), .B(sel), .Z(w4));
+NOT #(.Tpdhl(TpdNot),  .Tpdlh(TpdNot)) g6 (.A(d0), .Z(w6));
 
-NOT #(.Tpdhl(1),  .Tpdlh(2)) g3 (.A(w5), .Z(w3));
-OR2 #(.Tpdhl(10), .Tpdlh(1)) g5 (.A(w7), .B(w8), .Z(w5));
-NOT #(.Tpdhl(1),  .Tpdlh(2)) g7 (.A(sel), .Z(w7));
-NOT #(.Tpdhl(1),  .Tpdlh(2)) g8 (.A(d1), .Z(w8));
+NOT #(.Tpdhl(TpdNot),  .Tpdlh(TpdNot)) g3 (.A(w5), .Z(w3));
+OR2 #(.Tpdhl(TpdOr), .Tpdlh(TpdOr)) g5 (.A(w7), .B(w8), .Z(w5));
+NOT #(.Tpdhl(TpdNot),  .Tpdlh(TpdNot)) g7 (.A(sel), .Z(w7));
+NOT #(.Tpdhl(TpdNot),  .Tpdlh(TpdNot)) g8 (.A(d1), .Z(w8));
 
 
 
