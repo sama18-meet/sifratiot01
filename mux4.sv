56e8ca537e3c3c6e7973a5f2a4ef955dbf268609
Sama Mazzawi <sama.mazz93@gmail.com>
2021-12-11 19:16:50 +0200

    Update mux4.sv

--- mux4.sv
+++ mux4.sv
@@ -8,15 +8,17 @@ module mux4 (
     output logic z           // Output
 );
 
-// Put your code here
-// ------------------
+parameter TpdOr = 1;
+parameter TpdXor = 1;
+parameter TpdNot = 1;
 
 logic out1;
 logic out2;
-mux2 m1 (.d0(d0),.d1(d1),.sel(sel[0]),.z(out1));
-mux2 m2 (.d0(d2),.d1(d3),.sel(sel[0]),.z(out2));
-mux2 m3 (.d0(out1),.d1(out2),.sel(sel[1]),.z(z));
 
-// End of your code
+
+mux2 #(.TpdNot(TpdNot), .TpdOr(TpdOr), .TpdXor(TpdXor)) m1 (.d0(d0),.d1(d1),.sel(sel[0]),.z(out1));
+mux2 #(.TpdNot(TpdNot), .TpdOr(TpdOr), .TpdXor(TpdXor)) m2 (.d0(d2),.d1(d3),.sel(sel[0]),.z(out2));
+mux2 #(.TpdNot(TpdNot), .TpdOr(TpdOr), .TpdXor(TpdXor)) m3 (.d0(out1),.d1(out2),.sel(sel[1]),.z(z));
+
 
 endmodule
