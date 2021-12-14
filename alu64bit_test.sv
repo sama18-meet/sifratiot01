49804c8fb019bad4edfe13fc441a551dbdad2c3f
Sama Mazzawi <sama.mazz93@gmail.com>
2021-12-14 15:24:51 +0200

    Update alu64bit_test.sv

--- alu64bit_test.sv
+++ alu64bit_test.sv
@@ -1,10 +1,28 @@
 // 64-bit ALU test bench template
 module alu64bit_test;
 
-// Put your code here
-// ------------------
+logic [63:0] a = {64{1'b1}};
+logic [63:0] b = {64{1'b0}};
+logic cin = 1'b0;
+logic [1:0] op = {1'b1, 1'b0};
+logic [63:0] s;
+logic cout;
 
 
-// End of your code
+alu64bit inst(
+	.a(a),
+	.b(b),
+	.cin(cin),
+	.op(op),
+	.s(s),
+	.cout(cout)
+);
+
+initial begin
+
+#2190
+cin = 1'b1;
+#2190;
+end
 
 endmodule
